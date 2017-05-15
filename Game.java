f8956b6e3537d32d8db24434d1cc36d78d7b76fd
Jimmy Chen <jzc8@cornell.edu>
2017-05-15 13:10:38 -0400

    Add logic for key presses

--- Game.java
+++ Game.java
@@ -199,7 +199,15 @@ public class Game extends Canvas {
 	 * point after waiting for an appropriate amount of time
 	 */
 	public void tryToFire() {
-
+		// check that we have waiting long enough to fire
+		if (System.currentTimeMillis() - lastFire < firingInterval) {
+			return;
+		}
+		
+		// if we waited long enough, create the shot entity, and record the time.
+		lastFire = System.currentTimeMillis();
+		ShotEntity shot = new ShotEntity(this,"SOMESPRITE",ship.getX()+10,ship.getY()-30);
+		entities.add(shot);
 	}
 	
 	/**
@@ -227,18 +235,89 @@ public class Game extends Canvas {
 	 */
 	private class KeyInputHandler extends KeyAdapter {
 		/** The number of key presses we've had while waiting for some keypress */
-		private int pressCount = -1;
+private int pressCount = 1;
 		
-		
+		/**
+		 * Notification from AWT that a key has been pressed. Note that
+		 * a key being pressed is equal to being pushed down but *NOT*
+		 * released. Thats where keyTyped() comes in.
+		 *
+		 * @param e The details of the key that was pressed 
+		 */
 		public void keyPressed(KeyEvent e) {
+			// if we're waiting for an "any key" typed then we don't 
+			// want to do anything with just a "press"
+			if (waitingForKeyPress) {
+				return;
+			}
+			
+			
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				leftPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rightPressed = true;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				firePressed = true;
+			}
 		} 
 
+		/**
+		 * Notification from AWT that a key has been released.
+		 *
+		 * @param e The details of the key that was released 
+		 */
 		public void keyReleased(KeyEvent e) {
+			// if we're waiting for an "any key" typed then we don't 
+			// want to do anything with just a "released"
+			if (waitingForKeyPress) {
+				return;
+			}
+			
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				leftPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rightPressed = false;
+			}
+			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				firePressed = false;
+			}
 		}
 
 
+		/**
+		 * Notification from AWT that a key has been typed. Note that
+		 * typing a key means to both press and then release it.
+		 *
+		 * @param e The details of the key that was typed. 
+		 */
 		public void keyTyped(KeyEvent e) {
+			// if we're waiting for a "any key" type then
+			// check if we've recieved any recently. We may
+			// have had a keyType() event from the user releasing
+			// the shoot or move keys, hence the use of the "pressCount"
+			// counter.
+			if (waitingForKeyPress) {
+				if (pressCount == 1) {
+					// since we've now received our key typed
+					// event we can mark it as such and start 
+					// our new game
+					waitingForKeyPress = false;
+					startGame();
+					pressCount = 0;
+				} else {
+					pressCount++;
+				}
+			}
+			
+			// if we hit escape, then quit the game
+			if (e.getKeyChar() == 27) {
+				System.exit(0);
+			}
 		}
+	}
 			
 		
 		
@@ -256,4 +335,4 @@ public class Game extends Canvas {
 		g.gameLoop();
 	}
 }
-}
+
