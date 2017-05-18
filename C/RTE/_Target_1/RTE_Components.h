023a06e8ee9e88cb378237905f483e27d54224c6
Meghan Chen <mc2254@cornell.edu>
2017-05-18 08:34:51 -0700

    ok it doesn't say fuckthis anymore

--- C/RTE/_Target_1/RTE_Components.h
+++ C/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'fuckthis' 
+ * Project: 'spaceinvaders' 
  * Target:  'Target 1' 
  */
 
